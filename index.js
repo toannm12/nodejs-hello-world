43b9b7479d1c292e7ad2a880b24955cb4603a96f
toannm12 <ngominhtoan1986@gmail.com>
2020-09-08 00:09:58 +0700

    add log

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-
+    console.log("response to http request");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 
